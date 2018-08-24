06ff018834da7359f06250e237d10d80d5688812
A.Rupesh <a.rupesh66@gmail.com>
2018-08-24 23:29:26 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import Github from './Github';
 import Header from './Components/Header';
+import Auth0Lock from 'auth0-lock';
 
 
 class App extends Component {
-  
+
   constructor(props){
     super(props);
 
@@ -17,25 +18,90 @@ class App extends Component {
     };
   }
 
-
   static defaultProps = {
-    clientID: Sz7rXtT2JeGMs18hSgGUOqeXXOIH4HPI,
+    clientID: 'Sz7rXtT2JeGMs18hSgGUOqeXXOIH4HPI',
     domain: 'rupesh1310.auth0.com'
   }
-componentWillMount(){
-  this.lock = new Auth0Lock(this.props.clientID, this.props.domain)
-  this.lock.on('authenticated', (authResult) => {
-    console.log(authResult);
-  });
-}
-  
-  
-  
+
+  componentWillMount(){
+    this.lock = new Auth0Lock(this.props.clientID, this.props.domain);
+
+    this.lock.on('authenticated', (authResult) => {
+      // console.log(authResult);
+
+      this.lock.getProfile(authResult.idToken, (error, profile) => {
+        if(error){
+          console.log(error);
+          return;
+        }
+        // console.log(profile);
+
+        this.setProfile(authResult.idToken, profile);
+
+      });
+
+    });
+    this.getProfile();
+  }
+
+  setProfile(idToken, profile){
+    localStorage.setItem('idToken', idToken);
+    localStorage.setItem('profile', JSON.stringify(profile));
+
+    this.setState({
+      idToken: localStorage.getItem('idToken'),
+      profile: JSON.parse(localStorage.getItem('profile'))
+    });
+  }
+
+  getProfile(){
+    if(localStorage.getItem('idToken') != null){
+      this.setState({
+        idToken: localStorage.getItem('idToken'),
+        profile: JSON.parse(localStorage.getItem('profile'))
+      }, () => {
+        console.log(this.state);
+      });
+    }
+  }
+
+
+  showLock(){
+    this.lock.show();
+  }
+
+
+  logout(){
+    this.setState({
+      idToken: '',
+      profile: ''
+    }, () => {
+      localStorage.removeItem('idToken');
+      localStorage.removeItem('profile');
+
+    });
+  }
+
+
+
   render() {
+    let gitty;
+
+    if(this.state.idToken){
+      gitty = <Github />
+    } else{
+      gitty = "Click on Login to view Github Viewer"
+    }
+
     return (
       <div className="App">
-        <Header/>
-        <Github/>
+        <Header
+          lock={this.lock}
+          idToken={this.state.idToken}
+          onLogout={this.logout.bind(this)}
+          onLogin={this.showLock.bind(this)}
+           />
+         {gitty}
       </div>
     );
   }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,20 +2,38 @@ import React, { Component } from 'react';
 import {Navbar, Nav, NavItem} from 'react-bootstrap';
 
 class Header extends Component {
-    render(){
-        return(
-            <Navbar>
-              <Navbar.Header>
-                <Navbar.Brand>
-                  Github Searcher
-                </Navbar.Brand>
-              </Navbar.Header>
-              <Nav>
-             <NavItem href="#">Login</NavItem>
-              </Nav>
-            </Navbar>
-          );
-        }
-      }
-      
-      export default Header;
+
+  onLogin(){
+    this.props.onLogin();
+  }
+
+  onLogout(){
+    this.props.onLogout();
+  }
+
+
+  render(){
+    let page;
+    if(this.props.idToken){
+      page = <NavItem onClick={this.onLogout.bind(this)} href="#"> Logout</NavItem>
+    } else{
+      page = <NavItem onClick={this.onLogin.bind(this)} href="#"> Login</NavItem>
+    }
+
+
+    return(
+      <Navbar>
+        <Navbar.Header>
+          <Navbar.Brand>
+            Github Searcher
+          </Navbar.Brand>
+        </Navbar.Header>
+        <Nav>
+          {page}
+        </Nav>
+      </Navbar>
+    );
+  }
+}
+
+export default Header;
